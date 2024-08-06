61422eea9606ca905595e87bfc0eef2329dd4418
Alfito <mhmmdalfitoo@gmail.com>
2024-08-06 17:14:07 +0700

    first

---
+++ resources/views/includes/admin/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-3 p-3 text-center bg-white text-sm text-gray-600 dark:text-gray-400 dark:bg-gray-800">
+  <footer class="text-center font-medium bg-blue-200 py-5">
+    © {{ now()->year }} Dibangun Oleh: <a rel="nofollow" href="https://www.instagram.com/infokejadian.parakanwetan/" title="CSS Templates">KKN TIM II UNDIP 2024 Parakan Wetan</a>
+  </footer>
+</footer>
